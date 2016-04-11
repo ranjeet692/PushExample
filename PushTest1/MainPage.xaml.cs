8940cfb2f3e8a8d405e034eb1d117845c88fcb4b
Vipul Maheshwari <vipul@uohyd.ac.in>
2016-04-11 21:55:07 +0530

    sample windows 10 universal app

---
+++ PushTest1/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Networking.PushNotifications;
+using System.Threading.Tasks;
+using System.Threading;
+using CB;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PushTest1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            /*
+
+            PushNotificationChannel result = sCode.Result;
+            string uri = result.Uri;*/
+            var obj = new CloudPush();
+  
+            Task sCode = Task.Run(async () =>
+            {
+                await obj.InitAsync();
+            });
+
+        }
+
+        private async Task<PushNotificationChannel> init()
+        {
+            PushNotificationChannel _channel;
+            _channel = await PushNotificationChannelManager.CreatePushNotificationChannelForApplicationAsync();
+            return _channel;
+        }
+    }
+}
+
+
